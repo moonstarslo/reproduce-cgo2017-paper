801d0461eb6f37dd8eede61a41942e596fc1df8b
fugelin <fugelin@stu.xjtu.edu.cn>
2024-04-23 09:41:25 -0700

    update analysis for bfs prefetch assembly code

--- program/bfs/analysis/bfs_pf.asm
+++ program/bfs/analysis/bfs_pf.asm
@@ -14,7 +14,7 @@ void BFS_kernel(int depth)
     adrp	x14, 3441000
     adrp	x15, 5a67000 
     adrp	x16, 492000
-    movk	w11, #0x98, lsl #16
+    movk	w11, #0x98, lsl #16    // 0x98965f -> w11
     add	    x13, x13, #0x970
     add	    x14, x14, #0xb60
     add	    x15, x15, #0x568
@@ -30,8 +30,8 @@ void BFS_kernel(int depth)
   label-1: 	
     add	    x1, x13, x9, lsl #2    // addr of bfs_queue[queue_start_ptr] -> x1
    	ldrsw	x0, [x1]               // bfs_queue[queue_start_ptr] -> x0
-   	cmp	    x9, x11                // compare queue_start_ptr with 38495, 38495 is a parameter   
-   	csel	x18, x9, x11, lt       // if queue_start_ptr < 38495, pref_start_ptr = queue_start_ptr, pref_start_ptr -> x18
+   	cmp	    x9, x11                // compare queue_start_ptr with 0x98965f, 0x98965f is a parameter   
+   	csel	x18, x9, x11, lt       // if queue_start_ptr < 0x98965f, pref_start_ptr = queue_start_ptr, pref_start_ptr -> x18
    	add	    x18, x13, x18, lsl #2  // addr of bfs_queue[pref_start_ptr] -> x18
    	add	    w3, w0, #0x1           // bfs_queue[queue_start_ptr] + 1 -> w3
    	ldrsw	x2, [x18, #128]        // bfs_queue[pref_start_ptr + 32] -> x2, 128 is a parameter
@@ -59,8 +59,8 @@ void BFS_kernel(int depth)
 
   label-4: 	
     ldur	w3, [x1, #-256]        // col_idx[i] -> w3
-   	cmp	    x18, x11               // compare i with 38495
-   	csel	x4, x18, x11, lt       // if i < 38495, pref_i = i, pref_i -> x4
+   	cmp	    x18, x11               // compare i with 0x98965f
+   	csel	x4, x18, x11, lt       // if i < 0x98965f, pref_i = i, pref_i -> x4
    	add	    x4, x15, x4, lsl #2    // addr of col_idx[pref_i] -> x4
    	ldrsw	x5, [x4, #128]         // col_idx[pref_i+128] -> x5
    	sxtw	x4, w3                 // col_idx[i] -> x4
